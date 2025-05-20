8efffc84c54e6870baa9d029b17dfb11f672f67b
Rajanipawar <89697532+Rajanipawar@users.noreply.github.com>
2025-05-20 20:56:54 +0530

    Add files via upload

--- Day_1/Welcome_message_generator.py
+++ Day_1/Welcome_message_generator.py
@@ -1,1 +1,20 @@
+# Welcome  message generator
 
+#Step 1 - Ask for user details using input function
+
+name = input("What is your name?")
+language = input("Which programming language will you prefer?")
+hobby = input("What is your hobby?")
+color = input("What is your favorite color? ")
+pet = input("Which pet do you have (cat/dog)?")
+pet_name = input("What is your pet name?")
+
+#Step 2 - Generate the message
+
+print("\n!!!.....Welcome message Generator.....!!!\n")
+print(f"Hello, {name} 👋🏼! Glad to meet you!")
+print(f"Welcome to the world of {language} Programming.")
+print(f"It's great to know that you love {hobby}.")
+print(f"Your favorite color is {color}! That's awesome! 🌈")
+print(f"\nI heard you have a {pet}! How wonderful! 🐱🐶. Your pet's name is {pet_name}, that's so cute! 😻🐾")
+print("Keep believing in yourself — you're capable of amazing things! 💫")
