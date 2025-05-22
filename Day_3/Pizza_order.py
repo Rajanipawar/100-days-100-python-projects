fe35b3194605142680c8a21c00dbac17859389b6
Rajanipawar <89697532+Rajanipawar@users.noreply.github.com>
2025-05-22 21:54:59 +0530

    Add files via upload

--- Day_3/Pizza_order.py
+++ Day_3/Pizza_order.py
@@ -1,1 +1,34 @@
+print("!!!.....Welcome to Coco Pizza Diliveries.....!!!")
 
+# Step 1 : Take user inputs 
+size = input("What size of pizza do you want? S, M, or L : ").upper()
+pepperoni = input("Do you want pepperoni? Y or N: ").upper()
+extra_cheese = input("Do you want extra cheese? Y or N : ").upper()
+
+#Step 2 :  Base calculation
+
+bill = 0
+
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+else:
+    print("Invalid pizza size selected!!")
+
+# Step 3 : Add-ons
+
+if pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+# Step 4 : Final Output
+
+print(f"\nYour total bill is : {bill}")
