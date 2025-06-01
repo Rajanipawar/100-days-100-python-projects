c911909a003cec0fb341a7b187d7428184aabf84
Rajanipawar <89697532+Rajanipawar@users.noreply.github.com>
2025-06-01 23:04:32 +0530

    Add files via upload

--- Day_13/BMI_calculator.py
+++ Day_13/BMI_calculator.py
@@ -1,1 +1,11 @@
+weight = int(input("What is your weight?"))
+height = int(input("What is you height?"))
 
+bmi = weight / (height ** 2)
+
+if bmi >= 25:
+    print("OverWeight.")
+elif bmi >= 18.5:
+    print("Normal Weight.")
+else:
+    print("UnderWeight")
